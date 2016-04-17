10039788fe322b59cf1924ccfd2d135c504a2a70
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-04-17 20:03:48 +0200

    feature(pages): move pages to resource views

--- classes/ColdTrick/TranslationEditor/PageHandler.php
+++ classes/ColdTrick/TranslationEditor/PageHandler.php
@@ -9,49 +9,39 @@ namespace ColdTrick\TranslationEditor;
  * @subpackage TranslationEditor
  */
 class PageHandler {
-	
+
 	/**
 	 * The page handler for the nice url's of this plugin
 	 *
-	 * @param array $page the url elements
+	 * @param array $segments the url elements
 	 *
 	 * @return bool
 	 */
-	public static function translationEditor($page) {
-		
-		$include_file = false;
-		$base_path = elgg_get_plugins_path() . 'translation_editor/pages/';
-		
-		switch ($page[0]) {
+	public static function translationEditor($segments) {
+
+		$page = array_shift($segments);
+		switch ($page) {
 			case 'search':
-				$include_file = "{$base_path}search.php";
-				
-				break;
+				echo elgg_view_resource('translation_editor/search');
+				return true;
+
 			default:
-				if (empty($page[0])) {
-					break;
+				if (empty($page)) {
+					$language = get_current_language();
+					$plugin_id = null;
+				} else {
+					$language = $page;
+					$plugin_id = array_shift($segments);
+					set_input('current_language', $language);
+					set_input('plugin', $plugin_id);
 				}
-				
-				// set language
-				set_input('current_language', $page[0]);
-				
-				// set (optional) plugin_id
-				if (!empty($page[1])) {
-					set_input('plugin', $page[1]);
-				}
-				
-				$include_file = "{$base_path}index.php";
-				break;
+
+				echo elgg_view_resource('translation_editor/index', [
+					'current_language' => $language,
+					'plugin_id' => $plugin_id,
+				]);
+				return true;
 		}
-		
-		if (!empty($include_file)) {
-			include($include_file);
-			return true;
-		}
-		
-		$current_language = get_current_language();
-		forward("translation_editor/{$current_language}");
-		
-		return true;
 	}
+
 }
