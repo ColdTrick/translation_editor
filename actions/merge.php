77ed04e98d2087d07940d0b612fdff1576755e3d
Jerome Bakker <jeabakker@coldtrick.com>
2017-11-16 15:06:05 +0100

    fixed: potential content encoding issue in merged file

--- actions/merge.php
+++ actions/merge.php
@@ -7,27 +7,28 @@ $current_language = get_input('current_language');
 $plugin = get_input('plugin');
 
 if (!translation_editor_is_translation_editor()) {
-	register_error(elgg_echo('transation_editor:action:translate:error:not_authorized'));
-	forward(REFERER);
+	return elgg_error_response(elgg_echo('transation_editor:action:translate:error:not_authorized'));
 }
-
-// We'll be outputting a text file
-header('Content-Type: text/plain');
-	
-// It will be called $lang.php
-header("Content-Disposition: attachment; filename={$current_language}.php");
 
 $translation = translation_editor_get_plugin($current_language, $plugin);
 $translation = $translation['current_language'];
 
-echo '<?php' . PHP_EOL;
-echo '/**' . PHP_EOL;
-echo ' * This file was created by Translation Editor v' . elgg_get_plugin_from_id('translation_editor')->getManifest()->getVersion() . PHP_EOL;
-echo ' * On ' . date('Y-m-d H:i') . '' . PHP_EOL;
-echo ' */' . PHP_EOL;
-echo PHP_EOL;
-echo 'return ';
-echo var_export($translation, true);
-echo ';' . PHP_EOL;
+$content = [];
+$content[] = '<?php';
+$content[] = '/**';
+$content[] = ' * This file was created by Translation Editor v' . elgg_get_plugin_from_id('translation_editor')->getManifest()->getVersion();
+$content[] = ' * On ' . date('Y-m-d H:i');
+$content[] = ' */' . PHP_EOL;
+$content[] = 'return ' . var_export($translation, true) . ';' . PHP_EOL;
+
+$content = implode(PHP_EOL, $content);
+
+// start output
+header('Content-Type: text/plain; charset=utf-8');
+header('Content-Disposition: Attachment; filename="' . $current_language . '.php"');
+header('Content-Length: ' . strlen($content));
+header('Pragma: public');
+
+echo $content;
 
 exit();
